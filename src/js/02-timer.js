f8478ce2be79d2bcbefc110d87b78e64312f5fd0
Konstantin-Kotenko <kostya.kotenkoo@gmail.com>
2022-05-07 19:22:52 +0300

    work_with_2_task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,33 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const start = document.querySelector('button');
+let startBtn = start.setAttribute('disabled', 'disabled');
+let timerId;
+
+const refs = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    if (selectedDates[0] <= options.defaultDate) {
+      return window.alert('Please choose a date in the future');
+    }
+    startBtn = start.removeAttribute('disabled');
+  },
+};
+
+console.log(options.defaultDate);
+
 function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
@@ -15,6 +42,5 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000));
-console.log(convertMs(140000));
-console.log(convertMs(24140000));
+// start.addEventListener('click', () => (timerId = setInterval(convertMs)), 1000);
+flatpickr('input#datetime-picker', options);
